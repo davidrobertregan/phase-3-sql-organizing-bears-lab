2884b8f280c5242ede751774454e925b54e6be6a
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-09-22 10:46:27 -0500

    commit tree 275198e0b600081dd1bf355f4f8ea223b3a03e98

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+    ("Mr. Chocolate", 49, "M", "Black", "Mild", 1), 
+    ("Rowdy", 30, "M", "Black", "Crazy", 1),
+    ("Tabitha", 24, "F", "Brown", "Mild", 1),
+    ("Sergeant Brown", 30, "M", "Brown", "Easy", 0),
+    ("Melissa", 20, "F", "Black", "Moderate", 1),
+    ("Grinch", 15, "M", "Black", "Crazy", 1),
+    ("Wendy", 44, "F", "Brown", "Moderate", 0),
+    (NULL, 32, "M", "Black", "Mild", 1);
